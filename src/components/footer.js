b48a078cd2def78ad73b3d1cd99ab5d8bf765856
connorAlex <alex.connor.cochrane@gmail.com>
2022-09-28 18:26:25 -0500

    added footer content

--- src/components/footer.js
+++ src/components/footer.js
@@ -8,7 +8,10 @@ class Footer extends Component {
 
     render() {
         return (
-            <div>This is a footer</div>
+            <div>
+                <div>Created by <a target="_blank" rel="noopener noreferrer" href = "https://github.com/connorAlex">Connor Cochrane</a></div>
+                <div>This project is a subset of The Odin Project's curriculum</div>
+            </div>
         );
     };
 }
