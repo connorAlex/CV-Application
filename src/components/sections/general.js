5bcaf994a39d7e28e7420b430422fa4d94fd168f
connorAlex <alex.connor.cochrane@gmail.com>
2022-09-29 11:53:29 -0500

    added general information form

--- src/components/sections/general.js
+++ src/components/sections/general.js
@@ -8,7 +8,31 @@ class General extends Component {
 
     render() {
         return (
-            <div></div>
+            <div>
+                <form>
+                    <label for="name">Name</label>
+                    <input 
+                        type="text"
+                        id="name"
+                        name="name"
+                        placeholder='Name'
+                    />
+                    <label for="email">Email</label>
+                    <input 
+                        type="email"
+                        id="email"
+                        name="email"
+                    />
+                    <label for="phoneNum">Phone Number</label>
+                    <input 
+                        type="tel"
+                        id="phoneNum"
+                        name="phoneNum"
+                        pattern='[0-9]{3}-[0-9]{2}-[0-9]{3}'
+                    />
+                </form>
+
+            </div>
         );
     };
 }
