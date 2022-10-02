9d98f5621bcb1e9e935e9fa077deb098f759b657
connorAlex <alex.connor.cochrane@gmail.com>
2022-10-02 15:51:24 -0500

    added intital cv component

---
+++ src/components/sections/cv.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class CV extends Component  {
+    constructor(props) {
+      super(props);
+    }
+  
+  
+    render() {
+      return (
+        
+        <div className='cv'>
+          
+        </div>
+      )
+    }
+  }
+  
+  export default CV;
+  
