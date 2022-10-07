d3b4bc4dca64e299ce7510e0f219b4d44e404138
connorAlex <alex.connor.cochrane@gmail.com>
2022-10-07 10:49:46 -0500

    update App

--- src/App.js
+++ src/App.js
@@ -2,24 +2,15 @@ import React, { Component } from 'react';
 import Header from './components/header.js';
 import Main from './components/main.js';
 import Footer from './components/footer.js';
-import CV from './components/sections/CV/cv.js'
 
-class App extends Component  {
-  constructor(props) {
-    super(props);
-  }
-
-
-  render() {
-    return (
-      
-      <div className='app'>
-        <Header />
-        <Main />
-        <Footer />
-      </div>
-    )
-  }
-}
+const App = () => {
+  return (
+    <div className='app'>
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
+};
 
 export default App;
