60da4798e2a473dc4ba1987e16602eb7c91d4f9e
connorAlex <alex.connor.cochrane@gmail.com>
2022-10-07 10:51:28 -0500

    update footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,19 +1,13 @@
-import React, {Component} from 'react';
+import React from 'react';
 
-class Footer extends Component {
-    
-    constructor(props) {
-        super(props);
-    }
+const Footer = () => {
+    return (
+        <div className='footer'>
+            <div>Created by <a target="_blank" rel="noopener noreferrer" href = "https://github.com/connorAlex">Connor Cochrane</a></div>
+            <div>This project is a subset of The Odin Project's curriculum</div>
+        </div>
+    );    
+};
 
-    render() {
-        return (
-            <div className='footer'>
-                <div>Created by <a target="_blank" rel="noopener noreferrer" href = "https://github.com/connorAlex">Connor Cochrane</a></div>
-                <div>This project is a subset of The Odin Project's curriculum</div>
-            </div>
-        );
-    };
-}
 
 export default Footer;
