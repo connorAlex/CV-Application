5fd7ad11bca458e2e79a1e0eeb1db2664aaea025
connorAlex <alex.connor.cochrane@gmail.com>
2022-10-10 10:27:06 -0500

    update forms to hooks

--- src/components/sections/form/education.js
+++ src/components/sections/form/education.js
@@ -1,84 +1,77 @@
-import React, {Component} from 'react';
+import React from 'react';
 
-class Education extends Component {
-    
-    // eslint-disable-next-line no-useless-constructor
-    constructor(props) {
-        super(props);
-    }
+const Education = ({info, onChange, onSubmit}) => {
 
-    render() {
-        const { info, onChange, onSubmit } = this.props;
-        return (
-            
-            <div className = "education">
-                <form id="educationExp" onSubmit={onSubmit} autoComplete='off'>
-                    <div>Education</div>
-                    <div>
-                        <label htmlFor='school'>School</label>
-                        <input
-                            type="text"
-                            id="school"
-                            name='school'
-                            value={info.school}
-                            onChange={onChange}
-                        />
-                    </div>
-                    <div>
-                        <label htmlFor="degree">Degree</label>
-                        <input
-                            type="text"
-                            id="degree"
-                            name="degree"
-                            value={info.degree}
-                            onChange={onChange}
-                        />
-                    </div>
-                    <div>
-                        <label htmlFor="degreeType">Degree Type</label>
-                        <input
-                            type="text"
-                            id="degreeType"
-                            name="degreeType"
-                            value={info.degreeType}
-                            onChange={onChange}
-                        />
-                    </div>
-                    <div>
-                        <label htmlFor="major">Major</label>
-                        <input
-                            type="text"
-                            id="major"
-                            name='major'
-                            value={info.major}
-                            onChange={onChange}
-                        />
-                    </div>
-                    <div>
-                        <label htmlFor='startDate'>Start Date</label>
-                        <input
-                            type="text"
-                            name='startDate'
-                            id="startDate"
-                            value={info.startDate}
-                            onChange={onChange}
-                        />
-                    </div>
-                    <div>
-                        <label htmlFor='gradDate'>Graduation Date</label>
-                        <input
-                            type="text"
-                            id="gradDate"
-                            name='gradDate'
-                            value={info.gradDate}
-                            onChange={onChange}
-                        />
-                    </div>
-                    <button type="submit">Add</button>
-                </form>
-            </div>
-        );
-    };
-}
+    return (
+        
+        <div className = "education">
+            <form id="educationExp" onSubmit={onSubmit} autoComplete='off'>
+                <div>Education</div>
+                <div>
+                    <label htmlFor='school'>School</label>
+                    <input
+                        type="text"
+                        id="school"
+                        name='school'
+                        value={info.school}
+                        onChange={onChange}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="degree">Degree</label>
+                    <input
+                        type="text"
+                        id="degree"
+                        name="degree"
+                        value={info.degree}
+                        onChange={onChange}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="degreeType">Degree Type</label>
+                    <input
+                        type="text"
+                        id="degreeType"
+                        name="degreeType"
+                        value={info.degreeType}
+                        onChange={onChange}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="major">Major</label>
+                    <input
+                        type="text"
+                        id="major"
+                        name='major'
+                        value={info.major}
+                        onChange={onChange}
+                    />
+                </div>
+                <div>
+                    <label htmlFor='startDate'>Start Date</label>
+                    <input
+                        type="text"
+                        name='startDate'
+                        id="startDate"
+                        value={info.startDate}
+                        onChange={onChange}
+                    />
+                </div>
+                <div>
+                    <label htmlFor='gradDate'>Graduation Date</label>
+                    <input
+                        type="text"
+                        id="gradDate"
+                        name='gradDate'
+                        value={info.gradDate}
+                        onChange={onChange}
+                    />
+                </div>
+                <button type="submit">Add</button>
+            </form>
+        </div>
+    );
+};
+
 
 export default Education;
--- src/components/sections/form/general.js
+++ src/components/sections/form/general.js
@@ -1,61 +1,52 @@
-import React, {Component} from 'react';
+import React from 'react';
 
-class General extends Component {
+const General = ({onChange}) => {
     
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
-
-        const { onChange } = this.props;
-
-        return (
-            <div className = "general">
-                <form id="generalExp" autoComplete='off'>
-                    <div>Personal</div>
-                    <div>
-                        <label htmlFor="name">Name</label>
-                        <input
-                            type="text"
-                            id="name"
-                            name="name"
-                            onChange={onChange}
-                        />
-                    </div>
-                    <div>
-                        <label htmlFor="email">Email</label>
-                        <input
-                            type="email"
-                            id="email"
-                            name="email"
-                            onChange={onChange}
-                        />
-                    </div>
-                    <div>
-                        <label htmlFor="location">Location</label>
-                        <input
-                            type="location"
-                            id="location"
-                            name="location"
-                            onChange={onChange}
-                        />
-                    </div>
-                    <div>
-                        <label htmlFor="phone">Phone Number</label>
-                        <input
-                            type="tel"
-                            id="phone"
-                            name="phone"
-                            pattern='[0-9]{3}-[0-9]{2}-[0-9]{3}'
-                            onChange={onChange}
-                        />
-                    </div>
-                </form>
-            
-            </div>
-        );
-    };
-}
+    return (
+        <div className = "general">
+            <form id="generalExp" autoComplete='off'>
+                <div>Personal</div>
+                <div>
+                    <label htmlFor="name">Name</label>
+                    <input
+                        type="text"
+                        id="name"
+                        name="name"
+                        onChange={onChange}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="email">Email</label>
+                    <input
+                        type="email"
+                        id="email"
+                        name="email"
+                        onChange={onChange}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="location">Location</label>
+                    <input
+                        type="location"
+                        id="location"
+                        name="location"
+                        onChange={onChange}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="phone">Phone Number</label>
+                    <input
+                        type="tel"
+                        id="phone"
+                        name="phone"
+                        pattern='[0-9]{3}-[0-9]{2}-[0-9]{3}'
+                        onChange={onChange}
+                    />
+                </div>
+            </form>
+        
+        </div>
+    );
+};
 
 export default General;
